dc8859d9c6ae86f55afdc1181ffad7221a2deb13
Alina <alina12g@yahoo.com>
2021-08-31 14:28:57 +0300

    js line 7 changed

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-let cart = document.getElementsByClassName('.cart-shop');
-let select = document.querySelector('.select');
+let cart = document.getElementsByClassName('cart-shopping');
+let select = document.getElementsByClassName('select');
 let button = document.getElementsByTagName('button');
 
-for ( let btn of button){
-    btn.addEventListener('click', (e) => {
-        let add = Numer(cart.getAttribute('data-count') || 0);
+for(let but of button){
+    but.addEventListener('click', (e)=>{
+        let add = Number(cart.getAttribute('data-count') || 0);
         cart.setAttribute('data-count', add +1);
         cart.classList.add('zero')
 
+        // image --animation to cart ---//
         let image = e.target.parentNode.querySelector('img');
         let span = e.target.parentNode.querySelector('span');
         let s_image = image.cloneNode(false);
-
         span.appendChild(s_image);
         span.classList.add("active");
-        setTimeout(() => {
+        setTimeout(()=>{
             span.classList.remove("active");
             span.removeChild(s_image);
-        }, 500);
+        }, 500); 
+        
+        
 
+        // copy and paste //
         let parent = e.target.parentNode;
         let clone = parent.cloneNode(true);
-
         select.appendChild(clone);
-        clone.lastElementChild.innerText = "Buy now!";
+        clone.lastElementChild.innerText = "Buy-now";
+        
         if (clone) {
-            cart.onclick = () => {
+            cart.onclick = ()=>{
                 select.classList.toggle('display');
-
-            }
+            }	
         }
     })
 }
